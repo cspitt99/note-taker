b65cd0f76bbd0d917d231fa4e520d01f0ae9e873
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-10-18 16:27:35 -0400

    added server.js and added empty routes files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(express.static("public"))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log("app is listening on Port https://localhost:" + PORT)
+})
